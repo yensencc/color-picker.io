29ef1d8b01c14a69b9ee8d7884b0392e89faefd2
yensencc <33790982+yensencc@users.noreply.github.com>
2020-06-08 14:49:38 -0400

    Add files via upload
    Repository files

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Practice  color picker
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/yensencc/pen/wvMKdjP](https://codepen.io/yensencc/pen/wvMKdjP).
+
+
---
+++ src/script.js
@@ -0,0 +1,27 @@
+function doColor(){
+  var canvas = document.getElementById("can");
+  var newBackColor = document.getElementById("clr").value;
+  canvas.style.backgroundColor = newBackColor;
+}
+
+
+function doSquare(){
+  canvas = document.getElementById("can");
+  ctx = canvas.getContext("2d");
+  
+  ctx.fillStyle = "black";
+  amOfRect= document.getElementById("slr").value;
+  
+  ctx .clearRect(0,0,canvas.width,canvas.height);
+
+  //ctx.fillRect(10, 10,amOfRect,amOfRect);
+  
+  for (var i = 0; i < amOfRect; i = i + 1) {
+    ctx.fillRect(40 * i, 50, 36, 36);
+  }
+
+
+ 
+}
+
+
